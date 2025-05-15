c5b89cf3301bd42f30c4c57441dc5481596e2d05
kananimaulik26 <kananimaulik26@gmail.com>
2025-05-15 11:16:13 +0530

    fix API issue

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_APP_API_URL;
 
 export const fetchLanguages = async (isAdmin) => {
   try {
-    const response = await axios.get(`${API_URL}/languages?isAdmin=${isAdmin}`);
+    const response = await axios.get(`${API_URL}/api/languages?isAdmin=${isAdmin}`);
     return response.data;
   } catch (error) {
     console.error("Error fetching languages:", error);
